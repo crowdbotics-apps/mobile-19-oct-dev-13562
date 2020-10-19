965ceece3bed312ee26a0e390041b194b71bbc2b
Crowdbotics <team@crowdbotics.com>
2020-10-19 08:01:13 +0000

    Update models

--- backend/home/migrations/0008_remove_customtext_titlef.py
+++ backend/home/migrations/0008_remove_customtext_titlef.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0007_customtext_titlef'),
+        ("home", "0007_customtext_titlef"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='customtext',
-            name='titlef',
+            model_name="customtext",
+            name="titlef",
         ),
     ]
